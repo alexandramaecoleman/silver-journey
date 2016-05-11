1804f0f15a2c5faa8734f5ad7a7c4d673b9e7f02
Alex <alexandramaecoleman@gmail.com>
2016-05-11 13:47:07 +0100

    authorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,3 +21,6 @@ def thank_you
   UserMailer.contact_form(@email, @name, @message).deliver_now
 end
 
+rescue_from CanCan::AccessDenied do |exception|
+  redirect_to main_app.root_url, :alert => exception.message
+end
